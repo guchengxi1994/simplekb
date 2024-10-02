1c96b1f39e7c199326c53d692e657707940d0170
壹零 <guchengxi1994@qq.com>
2024-10-02 18:32:55 +0800

    Update JsonParseUtil.java

--- src/main/java/org/xiaoshuyui/simplekb/common/JsonParseUtil.java
+++ src/main/java/org/xiaoshuyui/simplekb/common/JsonParseUtil.java
@@ -8,18 +8,18 @@ public class JsonParseUtil {
     // 创建Gson对象
     static Gson gson = new Gson();
 
-    private static String removeMarkdown(String input) {
+    public static String removeMarkdown(String input) {
         // 去除反引号和换行符
         return input.replaceAll("`+", "").replaceAll("\\n", "");
     }
 
     // 解析JSON字符串的方法
-    private static JsonObject parseJson(String jsonString) {
+    public static JsonObject parseToJsonObject(String jsonString) {
         // 使用Gson解析JSON字符串
         return gson.fromJson(removeMarkdown(jsonString), JsonObject.class);
     }
 
-    private <T> T parseJson(String jsonString, Class<T> clazz) {
+    public static  <T> T parseToClass(String jsonString, Class<T> clazz) {
         // 使用Gson解析JSON字符串
         return gson.fromJson(removeMarkdown(jsonString), clazz);
     }
